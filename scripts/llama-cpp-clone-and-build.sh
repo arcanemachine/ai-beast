7da5eb8ccc09fd6c9f379ba27a4b934f180bdebf
Nicholas Moen <arcanemachine@gmail.com>
2026-02-14 15:03:39 -0700

    wip

--- scripts/llama-cpp-clone-and-build.sh
+++ scripts/llama-cpp-clone-and-build.sh
@@ -4,4 +4,4 @@ git clone "https://github.com/ggerganov/llama.cpp/" $current_date
 
 cd $current_date
 
-cmake -S . -B build -DGGML_HIP=ON -DGPU_TARGETS=gfx906 -DCMAKE_BUILD_TYPE=Release -DGGML_HIP_ROCWMMA_FATTN=ON && cmake --build build --parallel $(nproc)
+cmake -S . -B build -DGGML_HIP=ON -DCMAKE_HIP_ARCHITECTURES=gfx906 -DCMAKE_BUILD_TYPE=Release -DGGML_BACKEND_DL=ON -DGGML_CPU_ALL_VARIANTS=ON && cmake --build build --parallel $(nproc)
