e2324a5ed700dd3adc892569efd2c596abfd1632
Nicholas Moen <arcanemachine@gmail.com>
2026-02-14 22:52:29 -0700

    wip

--- scripts/llama-cli.sh
+++ scripts/llama-cli.sh
@@ -1,1 +1,7 @@
-../../llama-cpp/current/build/bin/llama-cli -m ../../models/Qwen3-Coder-Next-Q4_0.gguf -ngl 999 -c 16384 --no-mmap --temp 0.1
+../../llama-cpp/current/build/bin/llama-cli \
+  -m ../../models/Qwen3-Coder-Next-Q4_0.gguf \
+  -ngl 999 \
+  -c 65536 \
+  -n 4096 \
+  --no-mmap \
+  --temp 0.1
--- scripts/llama-server.sh
+++ scripts/llama-server.sh
@@ -1,9 +1,8 @@
 ../../llama-cpp/current/build/bin/llama-server \
   -m ../../models/Qwen3-Coder-Next-Q4_0.gguf \
   -ngl 999 \
-  -c 16000 \
-  -n 4000 \
+  -c 65536 \
+  -n 8192 \
   --no-mmap \
-  --cpu-moe \
   --temp 0.1 \
   --host 0.0.0.0
