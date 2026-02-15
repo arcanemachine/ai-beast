d7318fd723b3c86496d71e1d6c710fa9a083c4ca
Nicholas Moen <arcanemachine@gmail.com>
2026-02-15 03:37:44 -0700

    wip

---
+++ scripts/llama-bench.sh
@@ -0,0 +1,1 @@
+../../llama.cpp/build/bin/llama-bench $@
--- scripts/llama-server.sh
+++ scripts/llama-server.sh
@@ -1,10 +1,12 @@
 ../../llama.cpp/build/bin/llama-server \
   -m ../../models/Qwen3-Coder-Next-Q4_0.gguf \
   -ngl 999 \
-  -c 65536 \
+  --ctx-size 120000 \
+  -c 120000 \
   -n 8192 \
   -b 2048 \
   --no-mmap \
   --parallel 4 \
   --temp 0.1 \
+  --top-p 0.9 \
   --host 0.0.0.0
