c85178ca694110d874c0a9ef73a45592a535dbf8
Nicholas Moen <arcanemachine@gmail.com>
2026-02-16 00:00:10 -0700

    wip

--- scripts/llama-cli.sh
+++ scripts/llama-cli.sh
@@ -1,5 +1,13 @@
+#!/bin/sh
+
+model_path="../../models/Qwen3-Coder-Next-Q4_0.gguf"
+
+if [ $1 != "" ]; then
+  model_path=$1
+fi
+
 ../../llama.cpp/build/bin/llama-cli \
-  -m ../../models/Qwen3-Coder-Next-Q4_0.gguf \
+  -m $model_path \
   -ngl 999 \
   -c 65536 \
   -n 4096 \
--- scripts/llama-server.sh
+++ scripts/llama-server.sh
@@ -1,12 +1,20 @@
+#!/bin/sh
+
+model_path="../../models/Qwen3-Coder-Next-Q4_0.gguf"
+
+if [ $1 != "" ]; then
+  model_path=$1
+fi
+
 ../../llama.cpp/build/bin/llama-server \
-  -m ../../models/Qwen3-Coder-Next-Q4_0.gguf \
+  -m $model_path \
   -ngl 999 \
-  --ctx-size 180000 \
-  -c 180000 \
+  --ctx-size 200000 \
+  -c 200000 \
   -n 8192 \
   -b 2048 \
   --no-mmap \
-  --parallel 3 \
+  --parallel 2 \
   --temp 0.1 \
   --top-p 0.9 \
   --host 0.0.0.0
